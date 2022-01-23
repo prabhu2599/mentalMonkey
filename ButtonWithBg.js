5af280533c01d88eaeb2d0f533b3909c9a3c7d87
terminatorPG <55758267+prabhu2599@users.noreply.github.com>
2022-01-23 23:29:25 +0530

    Create ButtonWithBg.js

---
+++ ButtonWithBg.js
@@ -0,0 +1,9 @@
+let b6=document.createElement("span");
+      let b6a=document.createElement("A");
+      b6a.addEventListener("click",function(){window.open("google.com")});
+      b6a.style.backgroundColor="#0A66EB";
+      b6a.className=cn;
+      b6a.textContent="Prabhu Gantayat Mirror";
+      b6.appendChild(b6a);
+
+//add this to buttons list.
